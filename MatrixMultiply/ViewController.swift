958276d4237f23836aec76534a519c765d495ea9
deejayfakiofficial <71965798+deejayfakiofficial@users.noreply.github.com>
2020-12-30 16:10:06 +0400

    Initial Commit

---
+++ MatrixMultiply/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MatrixMultiply
+//
+//  Created by Stanislav Terentyev on 30.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
