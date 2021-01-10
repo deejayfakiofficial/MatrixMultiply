085fe222004519c577837c0aaa65ae7d2e395777
deejayfakiofficial <71965798+deejayfakiofficial@users.noreply.github.com>
2021-01-10 13:46:01 +0400

    add unit test

--- MatrixMultiply/ViewController.swift
+++ MatrixMultiply/ViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import MetalPerformanceShaders
 
 class ViewController: UIViewController {
     
@@ -15,31 +14,39 @@ class ViewController: UIViewController {
         let matrixOne = newMatrix()
         let matrixTwo = newMatrix()
         let matrixThree = multiplyMatrix(matrixOne, matrixTwo)
-            print(matrixThree)
+        for i in matrixThree {
+            DispatchQueue.main.async {
+                print(i)
+            }
+        }
     }
     
     func multiplyMatrix (_ matrixOne: [[Int]], _ matrixTwo: [[Int]]) -> [[Int]] {
         let size = matrixOne.count
         var result: [[Int]] = [[Int]](repeating: [Int](repeating: 0, count: size), count: size)
+        print("Final matrix is: ")
         for i in 0..<result.count {
-                for j in 0..<matrixOne.count {
-                    for k in 0..<matrixTwo.count {
-                        result[i][j] += matrixOne[i][k] * matrixTwo[k][j]
-                    }
+            for j in 0..<matrixOne.count {
+                for k in 0..<matrixTwo.count {
+                    result[i][j] += matrixOne[i][k] * matrixTwo[k][j]
                 }
             }
+//            let queue = DispatchQueue.global()
+//            queue.async {
+//                print(result[i])
+//            }
+        }
         return result
     }
     
     func newMatrix () -> [[Int]] {
         var matrix: [[Int]] = []
         for i in 0..<100 {
-            matrix.append([Int(arc4random_uniform(10))])
+            matrix.append([Int(arc4random_uniform(3))])
             for _ in 0..<100 {
-                matrix[i].append(contentsOf: [Int(arc4random_uniform(10))])
+                matrix[i].append(contentsOf: [Int(arc4random_uniform(3))])
             }
         }
         return matrix
     }
-    
 }
