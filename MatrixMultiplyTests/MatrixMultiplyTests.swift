1cfbd16f4f536c016907de938ee00c8e5ee8fb4d
deejayfakiofficial <71965798+deejayfakiofficial@users.noreply.github.com>
2021-01-18 11:40:09 +0400

    pull request

--- MatrixMultiplyTests/MatrixMultiplyTests.swift
+++ MatrixMultiplyTests/MatrixMultiplyTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import MatrixMultiply
 
 class MatrixMultiplyTests: XCTestCase {
-
+ //тест на алгоритм
     func test_multiplyMatrix () {
         let vc = ViewController()
         XCTAssertEqual(vc.multiplyMatrix([[1,2,3],[1,2,3],[1,2,3]], [[1,1,1],[2,2,2],[3,3,3]]), [[14,14,14],[14,14,14],[14,14,14]])
